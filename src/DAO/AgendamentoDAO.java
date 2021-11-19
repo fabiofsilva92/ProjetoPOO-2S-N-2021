d1e512e00029bde90d02d6b5cea068d2dd8172ee
unknown <fabiofsilva92@gmail.com>
2021-11-19 15:42:17 -0300

    Implementar DAO

--- src/DAO/AgendamentoDAO.java
+++ src/DAO/AgendamentoDAO.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface AgendamentoDAO {
 
+
     void adicionar(Agendamento a);
 
     List<Agendamento> pesquisarPorNome(String nome);
--- src/DAO/AgendamentoDAOImplements.java
+++ src/DAO/AgendamentoDAOImplements.java
@@ -5,6 +5,20 @@ import entity.Agendamento;
 import java.util.List;
 
 public class AgendamentoDAOImplements implements AgendamentoDAO {
+
+    private static final String DBURL = "jdbc:mariadb://localhost/POO_ACADEMIA";
+    private static final String DBUSER = "root";
+    private static final String DBPASS = "";
+
+
+    public AgendamentoDAOImplements(){
+        try{
+            Class.forName("org.mariadb.jdbc.Driver");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void adicionar(Agendamento a) {
 
